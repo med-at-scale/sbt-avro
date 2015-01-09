5898feca73f00cb6e9c614d16a1cbe29a3b2413e
Andy Petrella <andy.petrella@gmail.com>
2015-01-09 21:28:58 +0100

    align some versions for ga4gh models

--- project/Build.scala
+++ project/Build.scala
@@ -7,15 +7,15 @@ object build extends Build {
         base = file("."),
         settings = Defaults.defaultSettings ++ Seq[Project.Setting[_]](
             organization := "com.cavorite",
-            version := "0.3.2",
+            version := "0.3.2-mas", //med at scale
             sbtPlugin := true,
             libraryDependencies ++= Seq(
-                    "org.apache.avro" % "avro" % "1.7.5",
-                    "org.apache.avro" % "avro-compiler" % "1.7.5"
+                    "org.apache.avro" % "avro" % "1.7.6",
+                    "org.apache.avro" % "avro-compiler" % "1.7.6"
             ),
-            scalaVersion := "2.10.2",
+            scalaVersion := "2.10.4",
             scalacOptions in Compile ++= Seq("-deprecation"),
-            crossScalaVersions := Seq("2.10.2"),
+            crossScalaVersions := Seq("2.10.4"),
             description := "Sbt plugin for compiling Avro sources",
 
             publishTo := Some(Resolver.url("sbt-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)),
